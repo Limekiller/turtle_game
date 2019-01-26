8e6aa5b11ba7d58df867f098ced9dd3173e58b0e
Bryce David Yoder <bdyoder@un004-imac16.gc.goshen.edu>
2019-01-26 11:48:37 -0500

    Smooth camera movement

--- turtle_game/Assets/Scripts/CameraControl.cs
+++ turtle_game/Assets/Scripts/CameraControl.cs
@@ -5,34 +5,69 @@ using UnityEngine;
 public class CameraControl : MonoBehaviour {
 
 	public float ScrollSpeed = 50;
+    private Camera camera;
+    public float cameraSize;
+    public float newCameraSize;
+    public Vector3 currentPos;
+    public Vector3 newPos;
 
 	// Use this for initialization
 	void Start () {
-		
+		camera = GetComponent<Camera>();
+        cameraSize = camera.orthographicSize;
+        newCameraSize = camera.orthographicSize;
+
+        currentPos = transform.position;
+        newPos = currentPos;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+        // Camera zoom
+        if (Input.GetAxis("Mouse ScrollWheel") > 0f) {
+            if (camera.orthographicSize > 1){  
+                newCameraSize--;
+            }
+        }  else if (Input.GetAxis("Mouse ScrollWheel") < 0f) {
+            if (camera.orthographicSize < 10) {
+                newCameraSize++;
+            }
+        }
+        if (cameraSize < newCameraSize - 0.05f || cameraSize > newCameraSize + 0.05f) {
+            cameraSize = Mathf.Lerp(Mathf.Clamp(cameraSize, 1, 10), newCameraSize, 0.1f);
+            camera.orthographicSize = cameraSize;
+        }
+
+        if (currentPos != newPos) {
+            currentPos = Vector3.Lerp(currentPos, newPos, 0.05f);
+            transform.position = currentPos;
+        }
 		if ( Input.mousePosition.x >= Screen.width *0.95)
          {
-             transform.Translate(Vector3.right * Time.deltaTime * ScrollSpeed);
+             //transform.Translate(Vector3.right * Time.deltaTime * ScrollSpeed);
+             newPos += transform.right * 5 * Time.deltaTime;
          }
 
-		if ( Input.mousePosition.x <= Screen.width *0.05)
+		if ( Input.mousePosition.x <= Screen.width * 0.05)
          {
-             transform.Translate(-Vector3.right * Time.deltaTime * ScrollSpeed);
+             //transform.Translate(-Vector3.right * Time.deltaTime * ScrollSpeed);
+            newPos += -transform.right * 5 *Time.deltaTime;
 
          }
 
 		if ( Input.mousePosition.y >= Screen.height *0.95)
          {
-             transform.Translate(Vector3.up * Time.deltaTime * ScrollSpeed);
+            //transform.Translate(Vector3.up * Time.deltaTime * ScrollSpeed);
+            newPos += transform.up * 5 * Time.deltaTime;
+
          }
 
 		if ( Input.mousePosition.y <= Screen.height *0.05)
          {
-             transform.Translate(-Vector3.up * Time.deltaTime * ScrollSpeed);
+             //transform.Translate(-Vector3.up * Time.deltaTime * ScrollSpeed);
+            newPos += -transform.up * 5 * Time.deltaTime;
+
          } 
 	}
-
 }
